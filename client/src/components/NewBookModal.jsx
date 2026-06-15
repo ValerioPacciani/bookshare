745bb231a5512ea495611305ba4030bccebe123c
ValerioPacciani <valerio.pacciani@gmail.com>
2026-06-15 11:38:33 +0200

    Component NewBookModal for adding new books

---
+++ client/src/components/NewBookModal.jsx
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import { X, CirclePlus } from "lucide-react"
+import axiosClient from "../api/axiosConfig";
+//TODO ISBN E CATEGORIE
+const NewBookModal = ({ onclose }) => {
+    const [title, setTitle] = useState("");
+    const [author, setAuthor] = useState("");
+    const [file, setFile] = useState(null); //img
+
+    const handleTitle = (e) => {
+        setTitle(e.target.value);
+    }
+    const handleAuthor = (e) => {
+        setAuthor(e.target.value);
+    }
+
+    const handleFile = (e) => {
+        setFile(e.target.files[0])
+
+    }
+
+    const handleSubmit = async () => {
+        //creo l'oggetto da inviare al backend
+        const fData = new FormData()
+        fData.append("title", title);
+        fData.append("author", author);
+        fData.append("imageCover", file);
+        //invio al backand
+        await axiosClient.post('/api/books', fData);
+
+    }
+
+
+
+    return (
+
+        <div className="fixed inset-0 bg-black/50 flex items-center justify-center">
+            <div className="bg-white rounded-lg p-6 w-full max-w-md relative gap-2" >
+                <button className="rounded-2xl  absolute top-1 right-1" onClick={onclose}>
+                    <X className="hover:text-red-600" />
+                </button>
+                <form onSubmit={handleSubmit}>
+                    <div className="flex flex-col items-center justify-center">
+                        <div>
+                            <p className="font-bold mb-2" >Add a new book</p>
+                        </div>
+                        <div className="w-full">
+                            <p className="text-center">Title</p>
+                            <input className="border-2 border-gray-600 rounded-sm w-full" value={title} onChange={handleTitle}></input>
+                        </div>
+                        <div className="w-full">
+                            <p className="text-center">Author</p>
+                            <input className="border-2 border-gray-600 rounded-sm w-full" value={author} onChange={handleAuthor}></input>
+                        </div>
+                        <div className=" mt-3">
+                            {/* nascondo il vero input, per applicargli della grafica */}
+                            <input
+                                type="file"
+                                accept="image/*"
+                                onChange={handleFile}
+                                className="hidden"
+                                id="coverInput"
+                            />
+                            <label
+                                htmlFor="coverInput"
+                                className="cursor-pointer flex items-center justify-center border-2 border-dashed border-gray-400 rounded-lg p-4 hover:border-blue-500 hover:bg-blue-50"
+                            >
+                                {file ? file.name : "Click for add a Cover Image"}
+                            </label>
+                        </div>
+                        <button type="submit" className="rounded-full mt-3">
+                            <CirclePlus className="w-8 h-8 hover:text-green-500" />
+                        </button>
+
+                    </div>
+                </form>
+            </div>
+        </div >
+
+    )
+
+
+
+};
+
+export default NewBookModal
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,10 +4,13 @@ import Bookcard from "../components/Bookcard";
 import { useEffect } from "react";
 import { useState } from "react";
 import axiosClient from "../api/axiosConfig";
+import NewBookModal from "../components/NewBookModal";
+import { Plus } from "lucide-react";
 
 const Home = () => {
   const [books, setBooks] = useState([]); //la lista di libri posseduta dall utente, inizia come array vuoto
   const [loading, setLoading] = useState(true); //lo stato della pagina, setta a false quando ho finito di montare i componenti
+  const [showmodal, setmodal] = useState(false);
 
   useEffect(() => {
     //questa è il fetch dei libri dell utente, devo farlo via async, perchè axios lo richiede, quindi sono costretto a creare l arrowfunction fetchRESP e chiamrla subido dopo
@@ -31,7 +34,17 @@ const Home = () => {
             author={b.author}
             coverImage={b.coverImage}
           ></Bookcard>
+
         ))}
+        <div className="relative group">
+          <button onClick={() => setmodal(true)} className="bg-gray-300 hover:bg-green-400 rounded-full">
+            <Plus />
+          </button>
+          <span className="absolute bottom-full left-1/2 -translate-x-1/2 mb-2 px-2 py-1 bg-gray-800 text-white text-xs rounded opacity-0 group-hover:opacity-100 transition-opacity whitespace-nowrap">
+            New Book
+          </span>
+        </div>
+        {showmodal && <NewBookModal onclose={() => setmodal(false)} />}
       </div>
     </div>
   );
