2ebaf7ee650777ac99ef2cfedc1218e4304b1264
ValerioPacciani <valerio.pacciani@gmail.com>
2026-07-30 01:13:36 +0200

    Fixed a small bug, that multer will not accpet web images format

--- client/src/api/axiosConfig.js
+++ client/src/api/axiosConfig.js
@@ -11,7 +11,7 @@ const axiosClient = axios.create({
 axiosClient.interceptors.request.use(config => {
     const token = localStorage.getItem('token');
     if (token) {
-        config.headers.Authorization = 'Bearer ' + token;
+        config.headers.Authorization = 'Bearer ' + token; //se esiste lo inserisce nel formato coretto che si aspetta la chiamata http, ovvero "bearer" + token appunto
     }
     return config;
 }
--- server/middleware/ImagesUploaderMiddleware.js
+++ server/middleware/ImagesUploaderMiddleware.js
@@ -7,7 +7,7 @@ const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: "bookshare",
-    allowed_formats: ["jpg", "jpeg", "png"],
+    allowed_formats: ["jpg", "jpeg", "png","webp"],
   },
 });
 
